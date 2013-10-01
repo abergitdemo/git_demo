0ed2dc410669ac42d263177beae20f95eebdd97e
Ren Diao <rrd09@aber.ac.uk>
2013-10-01 14:27:19 +0100

    removed all prints from main

--- dev/src/Main.java
+++ dev/src/Main.java
@@ -2,9 +2,6 @@ public class Main {
 
 	public static void main(String[] args) {
 
-		System.out.println("Conflicting edit never gets old.");
-		System.out.println("Here I changed it again locally ... ");
-
 	}
 
 }
