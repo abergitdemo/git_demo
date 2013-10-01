0f2ca76101bc0b252155fc7d1bf9151bf98d9532
Ren Diao <rrd09@aber.ac.uk>
2013-10-01 10:38:17 +0100

    archive before dumbing down contents

---
+++ code/fifteen/src/Main.java
@@ -0,0 +1,25 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: rrd09
+ * Date: 30/09/13
+ * Time: 13:16
+ */
+public class Main {
+
+    public static void main(String[] args) {
+//        System.out.println("Hello World!");
+        System.out.print(Alphabet.getCharacter(108));
+        System.out.print(Alphabet.getCharacter(5));
+        System.out.print(Alphabet.getCharacter(12));
+        System.out.print(Alphabet.getCharacter(12));
+        System.out.print(Alphabet.getCharacter(15));
+        System.out.print(Alphabet.getCharacter(-1));
+        System.out.print(Alphabet.getCharacter(123));
+        System.out.print(Alphabet.getCharacter(15));
+        System.out.print(Alphabet.getCharacter(18));
+        System.out.print(Alphabet.getCharacter(12));
+        System.out.print(Alphabet.getCharacter(4));
+        System.out.println(Alphabet.getCharacter(-4));
+    }
+
+}
