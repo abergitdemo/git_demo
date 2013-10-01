2ca57bfbdf1e3b0da6f043d80a08774e4cbdbcfe
Ren Diao <endario@users.noreply.github.com>
2013-10-01 14:12:37 +0100

    another remote conflict

--- dev/src/Main.java
+++ dev/src/Main.java
@@ -2,7 +2,7 @@ public class Main {
 
 	public static void main(String[] args) {
 
-		System.out.println("Hello from GitHub ... try two.");
+		System.out.println("Conflicting edit never gets old.");
 		System.out.println("Here I changed it again locally ... ");
 
 	}
